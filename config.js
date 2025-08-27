9886775eb013395f9b1b667766ac489fdaa24e12
CarlosMacedo202 <carloshenriquemacedodesa@gmail.com>
2025-08-27 09:37:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
-const SUPABASE_URL = "https://vshmpjeklylzhmqzsuer.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZzaG1wamVrbHlsemhtcXpzdWVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyOTIzMjMsImV4cCI6MjA3MTg2ODMyM30.M0AZPymrv2Gm7aHbq8_cWvz2xfV5la0xOYGqg3SGjk8";
+const SUPABASE_URL = "https://hhybmnemvrvwbwktsnmt.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhoeWJtbmVtdnJ2d2J3a3Rzbm10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYyOTc0OTgsImV4cCI6MjA3MTg3MzQ5OH0.hxIhwfY-1jSfFyYrG3EgcTeYLGnKkEyEGqSjf89YlPo";
 const supabaseClient = supabase.createClient(SUPABASE_URL,
 SUPABASE_KEY);
